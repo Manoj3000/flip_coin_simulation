676190f5e5a270a18da2530815bd6fab84097ec0
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-27 13:06:12 +0530

    Added: flip coin multiple times to get winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,12 +2,30 @@
 
 echo "Welcome to flip coin simulator program"
 
-res=$((RANDOM%2))
+heads=0
+tails=0
 
-if [ $res -eq 0 ]
+for (( i=1; i<=10; i++ ))
+do
+    res=$((RANDOM%2))
+
+    if [ $res -eq 0 ]
+    then
+            ((heads++))
+    else
+            ((tails++))
+    fi
+done
+
+echo "Heads = $heads"
+echo "tails = $tails"
+
+if [ $heads -eq $tails ]
 then
-        echo "Heads is winner"
+        echo "Match tied"
+elif [ $heads -gt $tails ] 
+then
+        echo "Heads win"
 else
-        echo "tails is winner"
+        echo "Tails win"
 fi
-
